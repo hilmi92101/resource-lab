f29301347a2f9cf32f0f5773bd19efba26a027d2
hilmiAzman <hilmi.azman@srglobal.com>
2021-08-04 00:25:46 +0800

    Initial Commit

---
+++ resources/js/components/About.vue
@@ -0,0 +1,31 @@
+<template>   
+    <div>  
+        <h1>About</h1>  
+        <router-link :to="{ name: 'home'}" class="btn btn-primary">Home</router-link>
+        <router-link :to="{ name: 'about'}" class="btn btn-primary">About</router-link>
+    </div>   
+</template>   
+<script>   
+    export default {   
+        components: {   
+        },   
+        props: {   
+        },  
+    	data() {   
+            return {   
+                   
+            }   
+        },    
+        created() {   
+        },   
+    	methods: {   
+              
+        },   
+        computed: {   
+        },   
+        filters: {   
+              
+        }   
+           
+    }   
+</script>
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,32 @@
+<template>   
+    <div>  
+        <h1>Home</h1>  
+
+        <router-link :to="{ name: 'home'}" class="btn btn-primary">Home</router-link>
+        <router-link :to="{ name: 'about'}" class="btn btn-primary">About</router-link>
+    </div>   
+</template>   
+<script>   
+    export default {   
+        components: {   
+        },   
+        props: {   
+        },  
+    	data() {   
+            return {   
+                   
+            }   
+        },    
+        created() {   
+        },   
+    	methods: {   
+              
+        },   
+        computed: {   
+        },   
+        filters: {   
+              
+        }   
+           
+    }   
+</script>
